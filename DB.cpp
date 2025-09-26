23238daabaec6d134e8bcf80137b89c8f03f1218
Toxicattack <kuxarchik007@mail.ru>
2025-09-26 15:44:42 +0300

    Добавлены объявления функций

--- DB.cpp
+++ DB.cpp
@@ -1,3 +1,41 @@
 #pragma once
 #include "DB.h"
 
+DB::DB(const std::string& db_name) {
+    if (sqlite3_open(db_name.c_str(), &db)) {
+        throw std::runtime_error("Can't open database: " + std::string(sqlite3_errmsg(db)));
+    }
+}
+
+DB::~DB() {
+    sqlite3_close(db);
+}
+
+void DB::addMessage(const std::string& sender, const std::string& receiver, const std::string& content) {
+    
+}
+
+std::vector<std::string> DB::getMessages(const std::string& user1, const std::string& user2) {
+
+}
+
+void DB::addUser(const std::string& login, const std::string& password, const std::string& name) {
+
+}
+
+bool DB::verifyUser(const std::string& login, const std::string& password) {
+    
+    return false;
+}
+
+std::vector<std::string> DB::getUserMessages(const std::string& login) {
+
+}
+
+std::vector<std::string> DB::getUserList() {
+    return {};
+}
+
+void DB::logAction(const std::string& action) {
+
+}
--- DB.h
+++ DB.h
@@ -1,5 +1,7 @@
 #include <sqlite3.h>
 #include <string>
+#include <mutex>
+#include <vector>
 
 class DB {
 public:
@@ -7,14 +9,26 @@ public:
     ~DB();
 
     //фиксация сообщения в БД
+    void addMessage(const std::string& sender, const std::string& receiver, const std::string& content);
     //получение списка сообщений
-    //
+    std::vector<std::string> getMessages(const std::string& user1, const std::string& user2);
 
 
     //добавление пользователя в БД
+    void addUser(const std::string& login, const std::string& password, const std::string& name);
     //проверка пользователя в БД
+    bool verifyUser(const std::string& login, const std::string& password);
     //получение истории сообщений
+    std::vector<std::string> getUserMessages(const std::string& login);
     //получение списка пользователей
+    std::vector<std::string> getUserList();
 
+    
+    //Логирование действий
+    void logAction(const std::string& action);
 
+private:
+    sqlite3* db;
+    std::mutex dbMutex;
+    
 };
